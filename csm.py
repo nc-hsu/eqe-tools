077c53d41f1fdcdad451fbbfd4c0137d9767cbfb
nc-hsu <nicholas.clemett@hsu-hh.de>
2022-01-04 09:44:09 +0100

    dialog to select folders

--- csm.py
+++ csm.py
@@ -6,26 +6,22 @@ references:
 Fajfar 2000
 Freeman 1998
 """
-
+import os
 import numpy as np
+import tkinter as tk
 from pathlib import Path
+from typing import Tuple
+from tkinter import filedialog
+from nptyping import NDArray, Float64 
 
 
-def load_pushover(file_name, folder_path, header=True):
+def load_pushover(file_name: str, folder_path: str,
+                  header: bool=True) -> NDArray[Float64]:
     
-    """ loads a two column csv file representing the pushover curve
-        of a structure. The following assumptions are made:
-        (1) the first column is the base shear data
-        (2) the second column is the displacement data
+    """load pushover curve into two np.arrays
 
-        Args:
-            file_name (str): name of file incl. file extension
-            folder_path (str/Path): path to folder where data is stored
-            header (bool, optional): the file contains a header row.
-                                    Defaults to True.
-
-        Returns:
-            [numpy_array]: Baseshear and Displacement vectors
+    Returns:
+        [list]: [description]
     """
     
     folder_path = Path(folder_path)
@@ -36,11 +32,43 @@ def load_pushover(file_name, folder_path, header=True):
             data = np.loadtxt(file, skiprows=1, delimiter=',')
         else:
             data = np.loadtxt(file, delimiter=',')
-
-    V = data[:,0]
-    D = data[:,1]
     
-    return V, D
+    return data
+
+
+def select_folder(title: str ='') -> str:
+    """opens dialog for folder selection
+
+    Args:
+        title (str, optional): title of window. Defaults to ''.
+
+    Returns:
+        str: folder path 
+    """
+    # opens a dialog windows explorer box to allow selection
+    # of a folder
+
+    root = tk.Tk()
+    root.withdraw()
+
+    if title != None:
+        path = filedialog.askdirectory(title=title)
+    else:
+        path = filedialog.askdirectory()
+
+    root.destroy()
+
+    return path
+
+
+def filepaths_from_folder(folder_path):
+    # obtains the filepaths of all the files in a folder
+
+    folder = Path(folder_path)
+    file_names = os.listdir(folder)
+    file_paths = [folder / f for f in file_names]
+
+    return file_paths
 
 
 def mdof_to_sdof(V, D, m, phi):
@@ -57,12 +85,15 @@ def load_spectra():
 
 
 if __name__ == "__main__":
-    folder = Path('C:/niccl/git_repos/eqe-tools')
-    file = 'test_pushover.csv'
 
-    V, D = load_pushover(file, folder)
-    
-    for v, d in zip(V[:10], D[:10]):
-        print(v,d)
+    # folder = Path('C:/niccl/git_repos/eqe-tools')
+    # file = 'test_pushover.csv'
 
+    # V, D = load_pushover(file, folder)
     
+    # for v, d in zip(V[:10], D[:10]):
+    #     print(v,d)
+
+    folder = select_folder(title="dude select a folder")
+    paths = filepaths_from_folder(folder)
+    print(paths)
